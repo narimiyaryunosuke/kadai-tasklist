8658cd6591ee68295cf1b64c5847681a6a20bea7
narimiya.ryunosuke <gpz9ra4@icloud.com>
2019-03-10 03:30:18 +0000

    DRY

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,4 +1,6 @@
 class TasksController <  ApplicationController
+  before_action :set_task, only: [:show, :edit, :update, :destroy]
+  
   def index
     @tasks = Task.all
   end
@@ -49,6 +51,9 @@ class TasksController <  ApplicationController
   
   private
   
+  def set_task
+    @task = Task.find(params[:id])
+  end
   def task_params
     params.require(:task).permit(:content)
   end
