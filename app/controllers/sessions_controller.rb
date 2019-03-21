f9fda5cfa082714bcffbce6ddf1112c44a9f3d23
narimiya.ryunosuke <gpz9ra4@icloud.com>
2019-03-21 11:33:45 +0000

    user login

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,38 @@
+class SessionsController < ApplicationController
+  def new
+  end
+
+  def create
+     email = params[:session][:email].downcase
+    password = params[:session][:password]
+    if login(email, password)
+      flash[:success] = 'ログインに成功しました。'
+      redirect_to @task
+    else
+      flash.now[:danger] = 'ログインに失敗しました。'
+      render 'new'
+    end
+  end
+  
+
+  def destroy
+    session[:task_id] = nil
+    flash[:success] = 'ログアウトしました。'
+    redirect_to root_url
+  end
+    
+  private
+  
+    
+  def login(email, password)
+    @task = Task.find_by(email: email)
+    if @task && @task.authenticate(password)
+      # ログイン成功
+      session[:user_id] = @task.id
+      return true
+    else
+      # ログイン失敗
+      return false
+    end
+  end
+end
